c0f1bb346299beea918a7b8828875fb209d34bc1
jonathan koren <jonathan@jonathankoren.com>
2016-07-03 23:07:41 -0700

    update twitterFavoriteDump

--- twitterFavoriteDump.py
+++ twitterFavoriteDump.py
@@ -22,18 +22,20 @@ pseudonymous_name = self_user.name
 to_refavorite_id = []
 favorites = api.GetFavorites(count=200)
 while len(favorites) > 0:
-  for fav in favorites
+  for fav in favorites:
     created_timestamp = fav.created_at  # Sat Jan 02 04:00:04 +0000 2016
     text = fav.text
     status_id = fav.id
-    for url in fav.urls:
-      u = urls.expanded_url
-    for m in fav.media:
-      expanded_url = m['expanded_url']
-      media_url = m['media_url']
-      media_url_https = m['media_url_https']
-      id_str = m['id_str']
-      large_media_url_https = m['media_url_https'] + ':large'
+    #for url in fav.urls:
+    #  u = url.expanded_url
+    if fav.media is not None:
+      for m in fav.media:
+        print m
+        expanded_url = m.expanded_url
+        media_url = m.media_url
+        media_url_https = m.media_url_https
+        id_str = str(m.id)
+        large_media_url_https = m.media_url_https + ':large'
 
     to_refavorite_id.append(status_id)
     api.DestroyFavorite(status=fav)
